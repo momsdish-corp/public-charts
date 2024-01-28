0ce5cd083910af2ad65123292826e4ae48a4e42d
timofey-drozhzhin <26501155+timofey-drozhzhin@users.noreply.github.com>
2024-01-28 02:37:35 -0600

    Add mariadb-dev for local mysql work

---
+++ charts/mariadb-dev/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Apply the chart
+helm upgrade --install mariadb-dev . \
+  --namespace=mariadb-dev \
+  --create-namespace \
+  --set container.env.MARIADB_ROOT_PASSWORD=password
+
+# Wait for the pod to be ready
+kubectl --namespace=mariadb-dev wait --for=condition=ready pod --selector=app.kubernetes.io/instance=mariadb-dev --timeout=120s
+
+# Try running commands
+MARIADB_DEV_POD="$(kubectl --namespace=mariadb-dev get pods -l app.kubernetes.io/name=mariadb-dev -o jsonpath="{.items[0].metadata.name}")"
+kubectl --namespace=mariadb-dev exec "$MARIADB_DEV_POD" -- mysql --user=root --password=password --host mariadb-dev-svc --execute="SHOW DATABASES;"
